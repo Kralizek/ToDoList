67740384e2104b0f8d4ccdf21d04b45a34fe0705
Renato Golia <renato.golia@outlook.com>
2020-11-02 14:33:21 +0100

    Added GRPC console client

---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using Service;
+using static Service.ToDo;
+using static Service.ToDoItem.Types;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new ToDoClient(channel);
+
+            var item = new ToDoItem
+            {
+                Title = "My first item",
+                Description = "Hello world",
+                DueDate = Timestamp.FromDateTimeOffset(DateTimeOffset.UtcNow.AddDays(7)),
+                Priority = Priority.Highest
+            };
+
+            item.Tags.Add("No idea");
+
+            await client.AddItemAsync(new AddRequest
+            {
+                Item = item
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
